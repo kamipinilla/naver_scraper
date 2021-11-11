029179c6d72d0436c735da3c90a41aa325d74f71
Kami Pinilla <kamipinillar@gmail.com>
2021-11-10 20:58:31 -0500

    rf

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,35 +1,3 @@
-console.log ('Ready')
-// const children = document.body.childNodes
-// console.log(children.length)
-// for (const child of children) {
-//     console.log(child)
-// }
-
-// setTimeout(() => {
-//     const items = []
-
-//     const content = document.getElementById('content')
-//     const section = content.children[2]
-//     const component = section.children[2]
-//     const rows = component.children
-
-//     const sampleRow = rows[1]
-//     const sampleRowChildren = sampleRow.children
-
-//     const translation = sampleRowChildren[0]
-//     const translationCont = translation.children[0]
-//     items.push(translationCont.outerHTML)
-
-//     const sentence = sampleRowChildren[1]
-//     const sentenceText = sentence.children[0]
-//     items.push(sentenceText.outerHTML)
-
-//     const source = sampleRowChildren[2]
-//     items.push(source.outerHTML)
-// }, 5 * 1000)
-
-// const html = document.documentElement.outerHTML
-
 async function postToMiddleServer(obj, apiPath) {
   const proxyServerUrl = 'https://cors-anywhere-jcpr.herokuapp.com'
   const middleServerUrl = 'https://cloze-naver-middle.herokuapp.com'
@@ -53,8 +21,6 @@ async function postToMiddleServer(obj, apiPath) {
     const restError = json
     throw Error(restError.message)
   }
-
-  closeTab()
 }
 
 function closeTab() {
@@ -66,13 +32,14 @@ function getContentElement() {
   return content
 }
 
-function onDataReady() {
+async function onDataReady() {
   const contentElement = getContentElement()
   const contentHtml = contentElement.outerHTML
   const htmlUpdate = {
     content: contentHtml
   }
-  postToMiddleServer(htmlUpdate, '/api/htmlContent')
+  await postToMiddleServer(htmlUpdate, '/api/htmlContent')
+  closeTab()
 }
 
 let checkDataInterval = setInterval(() => {
