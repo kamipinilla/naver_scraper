cd77ee2cc9fd2680a315c11999087268e942352a
Kami Pinilla <kamipinillar@gmail.com>
2021-11-15 02:15:22 -0500

    .

--- client/src/blocks/word/Word.tsx
+++ client/src/blocks/word/Word.tsx
@@ -8,6 +8,21 @@ import { NaverExample } from '../../types'
 import Button from '../../widgets/Button'
 import H from '../../widgets/H'
 
+function getNaverExampleKey(naverExample: NaverExample): string {
+  return naverExample.sentPair.targetSent + "/" + naverExample.sentPair.sourceSent
+}
+
+function shortenOrigin(origin: string): string {
+  switch (origin) {
+    case "Oxford Advanced Learner's English-Korean Dictionary": {
+      return 'Oxford'
+    }
+    default: {
+      return origin
+    }
+  }
+}
+
 const WordComponent: React.FC = () => {
   const wordId: WordId = useNumberParam('wordId')
   
@@ -23,21 +38,28 @@ const WordComponent: React.FC = () => {
   useEffect(loadSelectedSentPairs, [loadSelectedSentPairs])
 
   const [naverExamples, setNaverExamples] = useState<NaverExample[] | null>(null)
-  console.log(naverExamples)
   const [page, setPage] = useState<number | null>(null)
   const [isScraping, setIsScraping] = useState<boolean>(false)
 
   const fetchNaverExamples = useCallback(() => {
     getHtmlContent().then(content => {
-      const naverExamples = extractNaverExamples(content)
-      setNaverExamples(naverExamples)
+      const fetchedNaverExamples = extractNaverExamples(content)
+      if (naverExamples === null) {
+        setNaverExamples(fetchedNaverExamples)
+        setPage(1)
+      } else {
+        const totalNaverExamples = naverExamples.concat(fetchedNaverExamples)
+        setNaverExamples(totalNaverExamples)
+        setPage(page! + 1)
+      }
     })
-  }, [])
+  }, [naverExamples, page])
 
   const scrapeNaver = useCallback((): void => {
     if (word !== null) {
       setIsScraping(true)
-      const naverUrl = getNaverUrl(word.name, page !== null ? page : 1)
+      const nextPage = page !== null ? (page + 1) : 1
+      const naverUrl = getNaverUrl(word.name, nextPage)
       window.open(naverUrl)
     }
   }, [word, page])
@@ -61,28 +83,40 @@ const WordComponent: React.FC = () => {
     return null
   }
 
-  const selectedSentPairsListItems = selectedSentPairs.map(sentPair => {
-    return (
-      <ul key={sentPair.id}>
-        <li>{sentPair.targetSent}</li>
-        <li>{sentPair.sourceSent}</li>
-      </ul>
-    )
-  })
-
   return (
     <div className="flex-col space-y-10">
       <div className="flex-col space-y-2 text-4xl">
         <div>{word.id}</div>
         <div>{word.name}</div>
       </div>
-      <div>
+      <Button onClick={scrapeNaver}>Load Naver</Button>
+      <div className="flex-col space-y-3">
         <H size="md">Selected</H>
         <div className="flex-col space-y-3">
-          {selectedSentPairsListItems}
+          {selectedSentPairs.map(sentPair => {
+            return (
+              <ul key={sentPair.id}>
+                <li>{sentPair.targetSent}</li>
+                <li>{sentPair.sourceSent}</li>
+              </ul>
+            )
+          })}
         </div>
       </div>
-      {naverExamples === null && <Button onClick={scrapeNaver}>Load Naver</Button>}
+      <div className="flex-col space-y-3">
+        <H size="md">Naver</H>
+        <div className="flex-col space-y-3">
+          {naverExamples !== null && naverExamples.map(naverExample => {
+            return (
+              <ul key={getNaverExampleKey(naverExample)}>
+                <li>{naverExample.sentPair.targetSent}</li>
+                <li>{naverExample.sentPair.sourceSent}</li>
+                <li>{shortenOrigin(naverExample.origin)}</li>
+              </ul>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
