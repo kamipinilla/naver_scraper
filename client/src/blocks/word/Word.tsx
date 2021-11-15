b4e7cd96d2d28351e302aff401fd9d0f2d5b077c
Kami Pinilla <kamipinillar@gmail.com>
2021-11-15 02:52:50 -0500

    check if already selected

--- client/src/blocks/word/Word.tsx
+++ client/src/blocks/word/Word.tsx
@@ -81,28 +81,34 @@ const WordComponent: React.FC = () => {
     }
   }, [onVisibilityChange])
 
-  const addNaverExampleToSelected = useCallback((naverExample: NaverExample): void => {
+  const naverExampleAlreadySelected = useCallback((naverExample: NaverExample): boolean => {
+    const naverTargetSent = naverExample.sentPair.targetSent
+    const naverSourceSent = naverExample.sentPair.sourceSent
+
     if (!selectedSentPairs) {
       throw Error("Can't add naver example without loaded selected sent pairs")
     }
 
-    const naverTargetSent = naverExample.sentPair.targetSent
-    const naverSourceSent = naverExample.sentPair.sourceSent
     const alreadySelected = selectedSentPairs.some(sentPair => {
       return sentPair.targetSent === naverTargetSent && sentPair.sourceSent === naverSourceSent
     })
 
-    if (alreadySelected) {
+    return alreadySelected
+  }, [selectedSentPairs])
+
+  const addNaverExampleToSelected = useCallback((naverExample: NaverExample): void => {
+    if (naverExampleAlreadySelected(naverExample)) {
       alert('Already selected')
       return
     }
+
     const newSentPair: NewSentPair = {
-      targetSent: naverTargetSent,
-      sourceSent: naverSourceSent,
+      targetSent: naverExample.sentPair.targetSent,
+      sourceSent: naverExample.sentPair.sourceSent,
     }
 
     addSentPair(wordId, newSentPair).then(fetchSelectedSentPairs)
-  }, [wordId, fetchSelectedSentPairs, selectedSentPairs])
+  }, [wordId, fetchSelectedSentPairs, naverExampleAlreadySelected])
 
   const removeSentPair = useCallback((sentPairId: SentPairId): void => {
     deleteSentPair(sentPairId).then(fetchSelectedSentPairs)
@@ -124,7 +130,7 @@ const WordComponent: React.FC = () => {
         <div className="flex-col space-y-3">
           {selectedSentPairs.map(sentPair => {
             return (
-              <ul key={sentPair.id} onClick={() => removeSentPair(sentPair.id)} className="bg-yellow-200 cursor-pointer p-2">
+              <ul key={sentPair.id} onClick={() => removeSentPair(sentPair.id)} className="bg-green-200 cursor-pointer p-2">
                 <li>{sentPair.targetSent}</li>
                 <li>{sentPair.sourceSent}</li>
               </ul>
@@ -136,8 +142,10 @@ const WordComponent: React.FC = () => {
         <H size="md">Naver</H>
         <div className="flex-col space-y-3">
           {naverExamples !== null && naverExamples.map(naverExample => {
+            const alreadySelected = naverExampleAlreadySelected(naverExample)
+            if (alreadySelected) return null
             return (
-              <ul key={getNaverExampleKey(naverExample)} onClick={() => addNaverExampleToSelected(naverExample)} className="bg-green-200 cursor-pointer p-2">
+              <ul key={getNaverExampleKey(naverExample)} onClick={() => addNaverExampleToSelected(naverExample)} className="bg-blue-200 cursor-pointer p-2">
                 <li>{naverExample.sentPair.targetSent}</li>
                 <li>{naverExample.sentPair.sourceSent}</li>
                 <li>{shortenOrigin(naverExample.origin)}</li>
